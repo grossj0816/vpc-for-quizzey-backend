692af20fe0d6c516fa6282d29d16b7c0bffa381e
Juwan Gross <juwangross0815@gmail.com>
2023-05-22 11:21:23 -0400

    Formatted tf files, updated vpc configs & added db tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 
   backend "s3" {
-    bucket = "tc-terraform-state-storage-s3"
-    key = "app-quizzey-networking"
-    region = "us-east-1"
+    bucket         = "tc-terraform-state-storage-s3"
+    key            = "app-quizzey-networking"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
 
@@ -32,25 +32,25 @@ resource "aws_vpc" "quizzey_app_vpc" {
 
 # Public Subnets
 resource "aws_subnet" "public_subnet" {
-  count = length(var.public_subnet_cidrs)
-  vpc_id = aws_vpc.quizzey_app_vpc.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.quizzey_app_vpc.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
   map_public_ip_on_launch = true
 
 
   tags = {
     Name = "Public Subnet ${count.index + 1}"
-  }  
+  }
 }
 
 
 # Lambda Subnets
 resource "aws_subnet" "lambda_subnet" {
-  count = length(var.lambda_subnet_cidrs) //setting how many lambda subnets we will create by count of cidr ranges given
-  vpc_id = aws_vpc.quizzey_app_vpc.id
-  cidr_block = element(var.lambda_subnet_cidrs, count.index) //retrieving specific cidr range from lambda_subnet_cidrs list
-  availability_zone = element(var.azs, count.index)  
+  count             = length(var.lambda_subnet_cidrs) //setting how many lambda subnets we will create by count of cidr ranges given
+  vpc_id            = aws_vpc.quizzey_app_vpc.id
+  cidr_block        = element(var.lambda_subnet_cidrs, count.index) //retrieving specific cidr range from lambda_subnet_cidrs list
+  availability_zone = element(var.azs, count.index)
 
 
   tags = {
@@ -61,9 +61,9 @@ resource "aws_subnet" "lambda_subnet" {
 
 # Database Subnets
 resource "aws_subnet" "database_subnet" {
-  count = length(var.database_subnet_cidrs) //setting how many lambda subnets we will create by count of cidr ranges given
-  vpc_id = aws_vpc.quizzey_app_vpc.id
-  cidr_block = element(var.database_subnet_cidrs, count.index) //retrieving specific cidr range from lambda_subnet_cidrs list
+  count             = length(var.database_subnet_cidrs) //setting how many lambda subnets we will create by count of cidr ranges given
+  vpc_id            = aws_vpc.quizzey_app_vpc.id
+  cidr_block        = element(var.database_subnet_cidrs, count.index) //retrieving specific cidr range from lambda_subnet_cidrs list
   availability_zone = element(var.azs, count.index)
 
 
@@ -84,18 +84,18 @@ resource "aws_internet_gateway" "igw" {
 
 # Elastic IP
 resource "aws_eip" "nat_eip" {
-  vpc =  true
-  depends_on = [ aws_internet_gateway.igw ]
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
 }
 
 # NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id = element(aws_subnet.public_subnet.*.id, 0) //we are returning the first  public subnet as the pool of ips that can be assiged to a private ip to request out to the internet.
-  depends_on = [ aws_internet_gateway.igw ]
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0) //we are returning the first  public subnet as the pool of ips that can be assiged to a private ip to request out to the internet.
+  depends_on    = [aws_internet_gateway.igw]
   tags = {
     Name = "Quizzey VPC NAT Gateway"
-  } 
+  }
 }
 
 
@@ -111,36 +111,67 @@ resource "aws_route_table" "private" {
 
 # Routes -----------------------------------------------------------------
 resource "aws_route" "internet_gw_route" {
-  route_table_id = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"  
-  gateway_id = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 
 resource "aws_route" "nat_gw_route" {
-  route_table_id = aws_route_table.private.id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 
 # Route Table Associations -----------------------------------------------
 resource "aws_route_table_association" "public_assoc" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
-  route_table_id =  aws_route_table.public.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
 
 resource "aws_route_table_association" "lambda_assoc" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id = element(aws_subnet.lambda_subnet.*.id, count.index)
-  route_table_id =  aws_route_table.private.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.lambda_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private.id
 }
 
 
 resource "aws_route_table_association" "database_assoc" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id = element(aws_subnet.database_subnet.*.id, count.index)
-  route_table_id =  aws_route_table.private.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.database_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+# Security Groups
+resource "aws_security_group" "db_sg" {
+  name        = "quizzey_db_sg"
+  description = "Security group for quizzey database"
+  vpc_id      = aws_vpc.quizzey_app_vpc.id
+
+  dynamic "ingress" {
+    iterator = cidr
+    for_each = var.lambda_subnet_cidrs
+    content {
+      from_port   = "3306"
+      to_port     = "3306"
+      protocol    = "tcp"
+      cidr_blocks = [cidr.value]
+    }
+  }
+
+  tags = {
+    Name = "Quizzey DB SG"
+  }
+}
+
+
+# DB Subnet Group --------------------------------
+resource "aws_db_subnet_group" "quizzey_db_subnet_group" {
+  name        = "quizzey_db_subnet_group"
+  description = "DB subnet group for Quizzey App"
+
+  subnet_ids = [for subnet in aws_subnet.database_subnet : subnet.id]
 }
